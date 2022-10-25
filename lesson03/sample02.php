82068410f14033bfeb2a0d4708fc9ef3eb604621
yamataso <yamataso0876@gmail.com>
2022-10-25 22:08:28 +0900

    ✨ sample02を作成しました。

---
+++ lesson03/sample02.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sample02</title>
+</head>
+<body>
+    <?php
+    $db = new mysqli("localhost:8889", "root", "root", "mydb");
+    $records = $db->query("select * from my_items");
+    if($records){
+        while($record = $records -> fetch_assoc()){
+            echo $record["item_name"] . "<br>";
+        }
+    }else{
+        echo $db->error;
+    }
+    ?>
+</body>
+</html>
