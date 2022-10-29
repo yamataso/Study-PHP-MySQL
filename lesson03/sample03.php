c2cf9521161d2a22b3824c7a26724a29dc445301
yamataso <yamataso0876@gmail.com>
2022-10-29 12:01:56 +0900

    ♻️ PHPでDBにデータを挿入しました

--- lesson03/sample03.php
+++ lesson03/sample03.php
@@ -1,22 +1,20 @@
 <!DOCTYPE html>
-<html lang="ja">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sample02</title>
+    <title>Sample03</title>
 </head>
 <body>
-    <?php
+<?php
     $db = new mysqli("localhost:8889", "root", "root", "mydb");
-    $records = $db->query("select count(*) as cnt from my_items");
-    if($records){
-        while($record = $records -> fetch_assoc()){
-            echo $record["cnt"] . "<br>";
-        }
-    }else{
+    $ret = $db->query('insert into memos (memo) values("PHPからのメモです")');
+    if ($ret):
+        echo "データを挿入しました";
+    else:
         echo $db->error;
-    }
+    endif;
     ?>
 </body>
 </html>
